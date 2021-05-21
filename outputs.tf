75f95a7d46d98c2861f449090e374cd95286d31c
Olivier Hoareau <oha@greenberets.io>
2021-05-21 11:53:05 +0200

    feat: output bucket name/arn

--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,9 @@ output "dns" {
 output "cloudfront_id" {
   value = module.website.cloudfront_id
 }
+output "bucket_name" {
+  value = module.website.bucket_name
+}
+output "bucket_arn" {
+  value = module.website.bucket_arn
+}
